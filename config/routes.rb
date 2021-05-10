ffb9ee83db22ee90aa4d5f7723b7703586821822
hugomtr <hugomaitre340@gmail.com>
2021-05-10 12:25:47 +0100

    ajout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'team', to: 'static#team'
+  get 'contact', to: 'static#contact'
+  get 'team/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
