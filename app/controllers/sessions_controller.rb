eecbb68de8c7d77e30a27cf20bb830ef270c05da
hugomtr <hugomaitre340@gmail.com>
2021-05-13 22:02:05 +0200

    #

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to '/gossip', notice: "Logged in!"
-    else
-      flash.now[:alert] = "Email or password is invalid"
-      render "new"
-    end
+    #if user #&& user.authenticate(params[:password])
+    #  session[:user_id] = user.id
+    redirect_to '/gossip' #, notice: "Logged in!"
+    #else
+    #  flash.now[:alert] = "Email or password is invalid"
+    #  render "new"
+    #end
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :gossips
   has_many :comments
   belongs_to :city
-  has_secure_password 
+  #has_secure_password 
   validates :password, presence: true, length: { minimum: 6 }
 end
