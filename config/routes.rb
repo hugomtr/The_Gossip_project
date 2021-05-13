eee76e80b24017a664c3e385c0988a100fb0e919
hugomtr <hugomaitre340@gmail.com>
2021-05-13 22:14:23 +0200

    #

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :likes 
   end
 
+  get '/contact', to: 'static#contact' 
+  get '/team' , to: 'static#team'
 
   post '/gossip', to: 'gossip#create', as: 'gossip_post' 
   
